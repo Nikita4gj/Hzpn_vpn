8449291988ab003269635335e70b2ae535c58388
Nikita Yavchenko <shoshiro.zutq@proton.me>
2026-09-22 20:01:58 +0300

    feat: add base project structure

---
+++ include/hzpn/version.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string_view>
+
+namespace hzpn {
+
+std::string_view version() noexcept;
+
+} 
---
+++ src/client/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "hzpn/version.hpp"
+
+int main() 
+{
+    std::cout << "hzpn_client " << hzpn::version() << std::endl;
+}
